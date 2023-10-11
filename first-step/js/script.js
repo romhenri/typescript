06e737a3cfab9b7c8b54ceac5cdf234a74348e45
Rômulo Henri <romulohenri000@gmail.com>
2023-10-11 18:53:43 -0300

    add: first-step

---
+++ first-step/js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+let msg = 'Hello World';
+console.log(msg);
+console.log(msg.toUpperCase);
+console.log(msg.toLowerCase);
+let string;
+let number;
+let element;
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(2, 3));
---
+++ first-step/script.ts
@@ -0,0 +1,15 @@
+let msg: string = 'Hello World';
+console.log(msg);
+
+console.log(msg.toUpperCase);
+console.log(msg.toLowerCase);
+
+let string: string;
+let number: number;
+let element: HTMLObjectElement;
+
+function sum (a:number, b:number) {
+  return a + b
+}
+
+console.log(sum(2, 3));
