3b8e7bc9932e333749c65ddffafeace6f4609897
Rômulo Henri <romulohenri000@gmail.com>
2023-10-28 22:42:33 -0300

    add: slide

---
+++ slide/dist/Slide.js
@@ -0,0 +1,12 @@
+export default class Slide {
+    constructor(container, elements, controls, time = 5000) {
+        this.container = container;
+        this.elements = elements;
+        this.controls = controls;
+        this.time = time;
+        console.log(this.container);
+        console.log(this.elements);
+        console.log(this.controls);
+        console.log(this.time);
+    }
+}
---
+++ slide/dist/script.js
@@ -0,0 +1,14 @@
+import Slide from "./Slide.js";
+console.log("Executando script!");
+const container = document.getElementById("slide");
+const elements = document.getElementById("slide-elements");
+const controls = document.getElementById("slide-controls");
+console.log(typeof container);
+console.log(typeof elements);
+console.log(typeof controls);
+if (container && elements && controls) {
+    new Slide(container, Array.from(elements.children), controls, 300);
+}
+else {
+    console.log("False");
+}
