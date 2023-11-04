1f004d866bb5a04d1d43c7f6b02f9a312ade84b8
Rômulo Henri <romulohenri000@gmail.com>
2023-11-03 22:44:38 -0300

    add: tuples

---
+++ tuples/dist/script.js
@@ -0,0 +1,14 @@
+"use strict";
+console.log("Testando...");
+const produto1 = ["Notebook", 2500];
+const produto2 = ["Notebook", 2500];
+console.log("produto1: ", produto1);
+console.log("produto2: ", produto2);
+console.log();
+// Errors
+// console.log(produto1[0].toLowerCase());
+// console.log(produto1[1].toFixed());
+// Right way with Tuples
+console.log(produto2[0].toLowerCase());
+console.log(produto2[1].toFixed());
+//# sourceMappingURL=script.js.map
